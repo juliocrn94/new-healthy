8db5c884091532ecc94f203bd60435374cb86de4
juliocrn94 <juliocrn94@gmail.com>
2020-03-07 12:01:52 -0600

    se agrega video y grid

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import "../css/index.css"
 
-class componentName extends Component {
+class Description extends Component {
     render () {
         return (
-            <div>
-                
+            <div className="descr-wrapper">
+                <div className="descr-video-wrapper">
+                    <video className="descr-video" controls>
+                        <source src="../images/video.mp4" type="video/mp4" />
+                    </video>
+                </div>
+                <div className="descr-texto">
+                    <p>Esta es la descripcion</p>
+                </div>
             </div>
         )
     }
 }
 
-export default componentName
+export default Description
