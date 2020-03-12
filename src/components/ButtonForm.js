410a0490e40925fe1c2a408868b7ba065325d8b4
fereber12 <fernando.reyes@wework.com>
2020-03-12 14:59:27 -0600

    correcciones estilos de botones

--- src/components/ButtonForm.js
+++ src/components/ButtonForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ButtonForm = props => {
     return (
-        <div className="botton-wrapper">
+        <div className="botton-wrap">
             <div className= {props.aligner}>
                 <button className="button button1" onClick= {props.onClick}>{props.text}</button>
             </div>
