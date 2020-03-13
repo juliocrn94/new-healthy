41b78c0e981b4c3795e19b1d1440a807af0788e9
juliocrn94 <52174255+juliocrn94@users.noreply.github.com>
2020-03-13 16:31:07 -0600

    finished table content

--- src/components/DashboardPlan.js
+++ src/components/DashboardPlan.js
@@ -16,59 +16,170 @@ const DashboardPlan = (props) => {
                 </thead>
                 <tr>
                 <td>Lunes</td>
-                <td>1 Yogurt con Ceral</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Yogurt con Cereal</p>
+                    <p>1 Rebanada de Pan Integral</p>
+                    <p>1 Feta de Queso Magro</p>
+                </td>
+                <td>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Porcion de Carne Roja</p>
+                    <p>1 Bowl de Ensalada</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Taza de Arroz</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
                 </tr>
+
                 <tr>
                 <td>Martes</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Yogurt descremado</p>
+                </td>
+                <td>
+                    <p>1 Filete de Pescado</p>
+                    <p>1 Porcion de Verduras Codias</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Porcion de Gelatina</p>
+                </td>
+                <td>
+                    <p>1 Plato de Spagueti con Tomate</p>
+                    <p>1 Fruta</p>
+                </td>
                 </tr>
                 <tr>
                 <td>Miercoles</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1/4 de Pollo</p>
+                    <p>1 Pure de Calabaza</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Yogurt con cereales</p>
+                    <p>1 Rebanada de Pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Ensalada de Arroz con Vegetales</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
                 </tr>
                 <tr>
                 <td>Jueves</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                    </td>
+                <td>
+                    <p>1 Yogurt Descremado</p>
+                </td>
+                <td>
+                    <p>1 Plato de Tartas de Verdura</p>
+                    <p>1 Ensalada Verde</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de pan Integral</p>
+                </td>
+                <td>
+                    <p>2 Plato de Tortilla con Huevo</p>
+                    <p>1 Ensalada de Tomate</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
                 </tr>
                 <tr>
                 <td>Viernes</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Cafe co leche descremada</p>
+                    <p>1 Rebanada de pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Yogurt Descremado</p>
+                </td>
+                <td>
+                    <p>1 Porcion de Tartas de Verduras</p>
+                    <p>1 Ensalada Verde</p>
+                </td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                </td>
+                <td>
+                    <p>2 Rodajas de Pescado Blanco</p>
+                    <p>1 Ensalada de Zanahoria rallada</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
                 </tr>
+
                 <tr>
                 <td>Sabado</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Vaso de Jugo de Naranja</p>
+                    <p>1 Cafe con Leche</p>
+                    <p>1 Fruta</p>
+                </td>
+                <td>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Caldo Desgrasado</p>
+                    <p>1 Ensalada de Verduras</p>
+                    <p>1 Postre</p>
+                </td>
+                <td>
+                    <p>1 Cafe Con Leche</p>
+                    <p>1 Fruta Fresca</p>
+                </td>
+                <td>
+                    <p>1 Caldo Desgrasado</p>
+                    <p>1 Ensalda de Frutas</p>
+                    <p>1 Gelatina</p>
+                </td>
                 </tr>
+
                 <tr>
                 <td>Domingo</td>
-                <td>1 Yogurt</td>
-                <td>1 Fruta Fresca</td>
-                <td>1 Porcion de Carne Roja</td>
-                <td>1 Cafe con Leche</td>
-                <td>1 Taza de Arroz</td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>1 Medialuna</p>
+                </td>
+                <td>
+                    <p>1 Gelatina</p>
+                </td>
+                <td>
+                    <p>1 Plato de Pasta con Salsa</p>
+                    <p>1 Bowl de Crezas Naturales</p>
+                </td>
+                <td>
+                    <p>1 Cafe con Leche</p>
+                    <p>2 Rebanadas de Pan Integral</p>
+                </td>
+                <td>
+                    <p>1 Filete de Pescado</p>
+                    <p>1 Bowl de Verduras</p>
+                    <p>3 Mitades de Durazno</p>
+                </td>
                 </tr>
             </table>
         </div>
