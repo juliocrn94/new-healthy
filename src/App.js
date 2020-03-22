6500f3e08f42028fbfd9c72a575011654e2db933
fereber12 <fernando.reyes@wework.com>
2020-03-21 19:19:59 -0600

    avances svg animation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Map from './components/Map';
 import Services2 from './components/Services2';
 import Testimonios from "./components/Testimonios";
 import Footer from './components/Footer';
+import Animacion from './components/Animacion';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./css/index.css";
 import "./css/Login.css";
 import "./css/Nav.css";
 import './App.css';
+import "./css/animacion.css";
 
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Animacion />
         <Description/>
         <Map /> 
         <Services2/>
---
+++ src/components/Animacion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../images/hb logo 1.svg';
+
+const Animacion = () => {
+  return (
+    <div class="svg-wrapper">
+      <img src= {Logo}/>
+    </div>
+  );
+};
+
+export default Animacion;
